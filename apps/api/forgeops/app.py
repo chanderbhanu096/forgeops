a2e76149647d6fe70a7fe03b8d0d46adf48b3524
Chander Bhanu <chanderbhanu096@gmail.com>
2026-08-03 21:35:28 +0200

    fix: allow Azure frontend origin

--- apps/api/forgeops/app.py
+++ apps/api/forgeops/app.py
@@ -60,7 +60,10 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:3000"],
+        allow_origins=[
+            "http://localhost:3000",
+            "https://forgeops-staging-web.greenrock-70958585.northeurope.azurecontainerapps.io",
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
