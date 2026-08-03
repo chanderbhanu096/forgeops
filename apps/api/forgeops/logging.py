51a7e1eb1d32f6914f52a3e4d72671bb8971389d
Chander Bhanu <chanderbhanu096@gmail.com>
2026-08-03 21:14:28 +0200

    fix: use stdlib logger factory for structlog

--- apps/api/forgeops/logging.py
+++ apps/api/forgeops/logging.py
@@ -33,7 +33,7 @@ def configure_logging() -> None:
         ],
         wrapper_class=structlog.make_filtering_bound_logger(log_level),
         context_class=dict,
-        logger_factory=structlog.PrintLoggerFactory(sys.stdout),
+        logger_factory=structlog.stdlib.LoggerFactory(),
         cache_logger_on_first_use=True,
     )
 
